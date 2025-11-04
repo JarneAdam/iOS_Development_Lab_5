7beec97af8baa9dc662882a41c628b71bd4d4938
JarneAdam <jarne.adam@student.vives.be>
2025-11-04 11:46:24 +0100

    Made project

---
+++ Lab_5/Lab_5/Lab_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_5App.swift
+//  Lab_5
+//
+//  Created by Jarne Adam on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
